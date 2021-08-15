de1908e615c2bd00252b4296ea23045259cc3d85
hacker173 <88987549+hacker173@users.noreply.github.com>
2021-08-15 23:35:48 +0300

    Add files via upload

---
+++ Bot/bot.py
@@ -0,0 +1,20 @@
+import config 
+import logging
+
+from aiogram import Bot, Dispacher, executor, types
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=config.API_TOCEN)
+dp = Dispacher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+	await message.answer(message.text)
+
+
+if __name__ == '__main__':
+	executor.start_polling(dp, skip_updates=True)
